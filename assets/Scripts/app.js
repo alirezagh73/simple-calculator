f268bcdca184c209ae3c2fa327a7b86b90613291
alireza gharghabi <alirza.gharghabi1@gmail.com>
2021-11-30 02:33:50 +0330

    create-dark-mode

--- assets/Scripts/app.js
+++ assets/Scripts/app.js
@@ -40,7 +40,6 @@ function clearDisplay() {
 }
 
 function mathCalc(mathFunc) {
-
     if (mathFunc === "pow") {
         flag = false;
         powFlag = true;
@@ -57,3 +56,29 @@ function mathCalc(mathFunc) {
     }
 
 }
+
+let flagMode = true;
+$("#mode_btn").click(function () {
+
+    if (flagMode) {
+        flagMode = false;
+        $(this).animate({
+            left: "55%"
+        })
+        document.documentElement.style.setProperty('--calc-dark-background', 'rgba(22,22,22,0.92)');
+        document.documentElement.style.setProperty('--calc-dark-btn-color', '#D8E9A8');
+        document.documentElement.style.setProperty('--calc-display-color', '#D8E9A8');
+        document.documentElement.style.setProperty('--mode-btn', 'rgba(22,22,22)');
+        document.documentElement.style.setProperty('--mode-btn-background', '#f0f2f8');
+    } else {
+        flagMode = true;
+        $(this).animate({
+            left: "-5%"
+        })
+        document.documentElement.style.setProperty('--calc-dark-background', '#f0f2f8');
+        document.documentElement.style.setProperty('--calc-dark-btn-color', 'linear-gradient(120deg, hsla(220, 13%, 87%, 0.9) 0%, hsla(220, 5%, 37%, 1) 100%)');
+        document.documentElement.style.setProperty('--calc-display-color', 'rgba(22,22,22,0.92)');
+        document.documentElement.style.setProperty('--mode-btn', '#f0f2f8');
+        document.documentElement.style.setProperty('--mode-btn-background', 'rgba(22,22,22,0.92)');
+    }
+})
